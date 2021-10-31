65a680a99e84a2c507b8e94b712f254457dd378a
Ryan <mccutcheon.ryan@icloud.com>
2021-10-31 14:56:00 +0100

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,11 +62,11 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_production"
 
-  #config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = false
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://git.heroku.com/desolate-gorge-59388.git'
+  host = 'https://git.heroku.com/desolate-gorge-59388.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
